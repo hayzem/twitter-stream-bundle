e44767cd80ef8a538777aa5c60078bdcfaa4521e
hayzem <aliatasever@gmail.com>
2017-02-03 23:04:41 +0700

    service test

---
+++ Service/TwitterStreamService.php
@@ -0,0 +1,11 @@
+<?php
+namespace Hayzem\TwitterStreamBundle\Service;
+/**
+ * @author Ali Atasever <aliatasever@gmail.com>
+ */
+class TwitterStreamService
+{
+    public function test(){
+        dump('twitter stream test');
+    }
+}
